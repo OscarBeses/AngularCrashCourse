b5ac6ab7b893f1498c1e67df6ce9ca18e8f9d4ca
INDRA\obeses <oscar.beses@gmail.com>
2020-12-02 12:41:22 +0100

    first commit

---
+++ src/app/authors/authors.component.ts
@@ -0,0 +1,20 @@
+import { AuthorService } from './../author.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent implements OnInit {
+
+  authors:string[];
+
+  constructor(service: AuthorService) { 
+    this.authors = service.getAuthors();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
