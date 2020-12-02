4fe14aa5edfd0862998fb3a41ea63f5c4267c707
INDRA\obeses <oscar.beses@gmail.com>
2020-12-02 19:22:17 +0100

    Se añade un custom pipe en el h1. Se añade el componente favorite.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+import { FavoriteComponent } from './favorite/favorite.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { AuthorService } from './author.service';
+import { TitleCasePipe } from './common/title-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthorsComponent
+    // Añadido manualmente
+    AuthorsComponent,
+    FavoriteComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule
