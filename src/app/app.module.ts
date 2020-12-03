e1f0c93148aef09758eb1806b66347ad471c1378
INDRA\obeses <oscar.beses@gmail.com>
2020-12-03 17:59:15 +0100

    Add new course form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,35 @@
-import { ZippyComponent } from './zippy/zippy.component';
-import { FavoriteComponent } from './favorite/favorite.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { ZippyComponent } from './zippy/zippy.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 import { AuthorService } from './author.service';
-import { TitleCasePipe } from './common/title-case.pipe';
 import { LikeComponent } from './like/like.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { TitleCasePipe } from './common/title-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     // Añadido manualmente
     AuthorsComponent,
-    FavoriteComponent,
     TitleCasePipe,
+    FavoriteComponent,
     LikeComponent,
-    ZippyComponent
+    ZippyComponent,
+    NewCourseFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    /* 
+     * Sin estas dos clases no van los template-driven forms 
+     * ni los model-driven forms 
+     */
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [AuthorService],
   bootstrap: [AppComponent]
